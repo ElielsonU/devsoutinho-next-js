53a08fdc35c99982e4366c4b326744c7b3a2ae1b
ElielsonU <elielson7756@gmail.com>
2023-03-29 14:12:18 -0300

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Você se perdeu e chegou na pagina 404</h1>
+
+            <Link href="/">
+                Ir para Home
+            </Link>
+        </div>
+    )
+} 
